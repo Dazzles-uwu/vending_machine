975658c1d8c236a9f32ad5e571b76eff040eeb17
Daffa Buckley <daffabuck@gmail.com>
2023-04-07 21:18:03 +1000

    Created function to display List of Products that must be displayed

--- main.py
+++ main.py
@@ -4,11 +4,15 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 class Item:
-    def __init__(self, item_name):
+    def __init__(self, item_name, item_stock, item_price):
         self.name = item_name
+        self.stock = item_stock
+        self.price = item_price
 
     def __str__(self):
-        return f"Item Type: {self.name}"
+        return f"Item Type: {self.name}\n" \
+               f"Stock Amt.: {self.stock}\n" \
+               f"Item Price: {self.price}\n"
 
 
 class Cart:
@@ -40,6 +44,25 @@ class Transaction:
                f"Payment amount: {self.payment}\n"
 
 
+# Add this in later
+def new_transaction():
+    while True:
+        print("Welcome to a new transaction")
+        break
+
+
+# HardCoded for now / U-002
+def display_list_of_products():
+    dict_of_items = {"coffee": ["3", "2.20"], "tea": ["0", "2.20"], "juice": ["200", "2.20"]}
+
+    print("\nList of Vending Machine Products")
+    for keys, values in dict_of_items.items():
+        if int(values[0]) <= 0:
+            print(f'{keys.capitalize():8} ALERT PRODUCT OUT OF STOCK')
+        else:
+            print(f'{keys.capitalize():8} Available Amount: {values[0]:5} Price: ${values[1]}')
+    print("\n")
+
 def main_menu_list():
     print("You are in the Main Menu!")
     print("Please choose one of the following options")
@@ -58,11 +81,19 @@ def display_main_menu():
         main_menu_list()
         choice = input("Select an Option: ")
 
-        if str(choice).lower() == "6":
+        if str(choice).lower() == "1":
+            # Start Transaction
+            new_transaction()
+        elif str(choice).lower() == "2":
+            # Display List
+            display_list_of_products()
+        elif str(choice).lower() == "3" or str(choice).lower() == "4" or str(choice).lower() == "5":
+            password_input = input("Admin Password (123): ")
+            # Continue depending on the variable "Choice"
+            # if password_input == "123":
+        elif str(choice).lower() == "6":
             print("Shutting Down now")
             menu_over = True
-        if str(choice).lower() == "3" or str(choice).lower() == "4" or str(choice).lower() == "5":
-            print("Admin Password (123): ")
 
 
 def main_menu():
