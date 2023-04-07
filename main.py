6b3669d9afbbbf0871856a233a917fdd952a8096
Daffa Buckley <daffabuck@gmail.com>
2023-04-07 20:12:21 +1000

    Finalised initial class skeleton

--- main.py
+++ main.py
@@ -4,35 +4,40 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 class Item:
-    def __init__(self, itemname):
-        self.name = itemname
+    def __init__(self, item_name):
+        self.name = item_name
 
     def __str__(self):
         return f"Item Type: {self.name}"
 
 
 class Cart:
-    def __init__(self, itemname):
-        self.name = itemname
+    def __init__(self, list_of_items):
+        self.cart_items = list_of_items
 
     def __str__(self):
-        return f"Item Type: {self.name}"
+        return f"Cart contains: {self.cart_items}"
 
 
 class Machine:
-    def __init__(self, itemname):
-        self.name = itemname
+    def __init__(self, status):
+        self.status = status
 
     def __str__(self):
-        return f"Item Type: {self.name}"
+        return f"Machine Status: {self.status}"
 
 
 class Transaction:
-    def __init__(self, itemname):
-        self.name = itemname
+    def __init__(self, items_purchased, date, payment):
+        self.items_purchased = items_purchased
+        self.date = date
+        self.payment = payment
 
     def __str__(self):
-        return f"Item Type: {self.name}"
+        return f"Transaction\n" \
+               f"Items Purchased: {self.items_purchased}\n" \
+               f"Date Purchased: {self.date}\n" \
+               f"Payment amount: {self.payment}\n"
 
 
 def main_menu_list():
